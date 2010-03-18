8317760213d21cf928ae154fc662e8cb2e16a810
Kenneth Waters <kwwaters@gmail.com>
2010-03-17 21:23:07 -0700

    Initial working quine.

---
+++ quine.c
@@ -0,0 +1,108 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* s[] = {
+  "",
+  "#include <stdio.h>",
+  "#include <stdlib.h>",
+  "#include <string.h>",
+  "",
+  "const char* s[] = {",
+  0,
+  "};",
+  "",
+  "int main(int argc, char ** argv) {",
+  "  int count = 0;",
+  "  char** line = s;",
+  "  char** cursor = s;",
+  "",
+  "  for (; *line; ++line)",
+  "    printf(\"%s\\n\", *line);",
+  "  ",
+  "  while (count < 2) {",
+  "    char* quoted = malloc(2 * strlen(*cursor));",
+  "    char* q = quoted;",
+  "    char* c;",
+  "    for (c = *cursor; *c; ++c) {",
+  "      switch (*c) {",
+  "        case '\\\\':",
+  "          *q++ = '\\\\';",
+  "          *q++ = '\\\\';",
+  "          break;",
+  "        case '\"':",
+  "          *q++ = '\\\\';",
+  "          *q++ = '\"';",
+  "          break;",
+  "        default:",
+  "          *q++ = *c;",
+  "      }",
+  "    }",
+  "    *q = '\\0';",
+  "",
+  "    printf(\"  \\\"%s\\\",\\n\", quoted);",
+  "    free(quoted);",
+  "    if (!*++cursor) {",
+  "      if (!count)",
+  "        printf(\"  0,\\n\");",
+  "      ++cursor;",
+  "      ++count;",
+  "    }",
+  "  }",
+  "  printf(\"  0\\n\", *cursor);",
+  "",
+  "  for (++line; *line; ++line)",
+  "    printf(\"%s\\n\", *line);",
+  "",
+  "  return 0;",
+  "}",
+  "",
+  0
+};
+
+int main(int argc, char ** argv) {
+  int count = 0;
+  char** line = s;
+  char** cursor = s;
+
+  for (; *line; ++line)
+    printf("%s\n", *line);
+  
+  while (count < 2) {
+    char* quoted = malloc(2 * strlen(*cursor));
+    char* q = quoted;
+    char* c;
+    for (c = *cursor; *c; ++c) {
+      switch (*c) {
+        case '\\':
+          *q++ = '\\';
+          *q++ = '\\';
+          break;
+        case '"':
+          *q++ = '\\';
+          *q++ = '"';
+          break;
+        default:
+          *q++ = *c;
+      }
+    }
+    *q = '\0';
+
+    printf("  \"%s\",\n", quoted);
+    free(quoted);
+    if (!*++cursor) {
+      if (!count)
+        printf("  0,\n");
+      ++cursor;
+      ++count;
+    }
+  }
+  printf("  0\n", *cursor);
+
+  for (++line; *line; ++line)
+    printf("%s\n", *line);
+
+  return 0;
+}
+
